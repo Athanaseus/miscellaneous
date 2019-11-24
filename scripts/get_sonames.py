304a13c23ce95e53d26727a8aee17e184f8fa4f1
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2019-11-24 11:40:05 +0200

    Update get_sonames.py

--- scripts/get_sonames.py
+++ scripts/get_sonames.py
@@ -1,3 +1,20 @@
+#dpkg -x libcasa-casa4_3.2.1-1kern1_amd64.deb /tmp/libcasa-casa
+#dpkg -x libcasa-images4_3.2.1-1kern1_amd64.deb /tmp/libcasa-images
+#dpkg -x libcasa-mirlib4_3.2.1-1kern1_amd64.deb /tmp/libcasa-mirlib
+#dpkg -x libcasa-python4_3.2.1-1kern1_amd64.deb /tmp/libcasa-python
+#dpkg -x libcasa-coordinates4_3.2.1-1kern1_amd64.deb /tmp/libcasa-coordinates
+#dpkg -x libcasa-lattices4_3.2.1-1kern1_amd64.deb /tmp/libcasa-lattices
+#dpkg -x libcasa-ms4_3.2.1-1kern1_amd64.deb /tmp/libcasa-ms
+#dpkg -x libcasa-scimath4_3.2.1-1kern1_amd64.deb /tmp/libcasa-scimath
+#dpkg -x libcasa-derivedmscal4_3.2.1-1kern1_amd64.deb /tmp/libcasa-derivedmscal
+#dpkg -x libcasa-meas4_3.2.1-1kern1_amd64.deb /tmp/libcasa-meas
+#dpkg -x libcasa-msfits4_3.2.1-1kern1_amd64.deb /tmp/libcasa-msfits
+#dpkg -x libcasa-scimath-f4_3.2.1-1kern1_amd64.deb /tmp/libcasa-scimath-f
+#dpkg -x libcasa-fits4_3.2.1-1kern1_amd64.deb /tmp/libcasa-fits
+#dpkg -x libcasa-measures4_3.2.1-1kern1_amd64.deb /tmp/libcasa-measures
+#dpkg -x libcasa-python3-4_3.2.1-1kern1_amd64.deb /tmp/libcasa-python3
+#dpkg -x libcasa-tables4_3.2.1-1kern1_amd64.deb /tmp/libcasa-tables
+
 import subprocess
 from glob import glob
 VERSION = '3.2.1'
