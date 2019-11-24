4c0af935d3d95ddce3a6ceb93bb181b37b4f1902
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2019-11-24 12:58:37 +0200

    save symbols in debian
    Shared libraries control files

--- scripts/get_sonames.py
+++ scripts/get_sonames.py
@@ -25,7 +25,7 @@ for lib in libs:
    package = lib.split('/')[-1]
    print("Package: {}".format(package))
    command1 = 'mkdir -p {0:s}{1:s}/DEBIAN'
-   command2 = 'dpkg-gensymbols -v{0:s} -p{1:s}{2:s} -e{3:s} -O{4:s}{5:s}/DEBIAN/symbols'
+   command2 = 'dpkg-gensymbols -v{0:s} -p{1:s}{2:s} -e{3:s} -O{4:s}{5:s}.symbols'
    command1 = command1.format(package, SOVERSION)
    command2 = command2.format(VERSION, package, SOVERSION, lib+file_name, package, SOVERSION)
    print('Running: {}'.format(command1))
